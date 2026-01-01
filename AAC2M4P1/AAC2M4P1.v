0d7478bb57415b73015996766449de69a8f1180e
CeSium <fedir.katushonok@gmail.com>
2026-01-01 09:26:56 +0200

    refactor LS161a module to improve output declaration and structure

--- AAC2M4P1/AAC2M4P1.v
+++ AAC2M4P1/AAC2M4P1.v
@@ -5,6 +5,28 @@ module LS161a(
     input LOAD_n,         // Enable Parallel Input
     input ENP,            // Count Enable Parallel
     input ENT,            // Count Enable Trickle
-    output [3:0]Q,        // Parallel Output 	
-    output RCO            // Ripple Carry Output (Terminal Count)
+    output reg [3:0]Q,        // Parallel Output 	
+    output reg RCO            // Ripple Carry Output (Terminal Count)
 ); 
+
+wire [3:0] combined = {CLR_n, LOAD_n, ENT, ENP};
+
+always @(posedge(CLK), negedge(CLR_n)) begin
+    case (combined)
+        4'b0xxx: begin
+            Q <= 4'b0000;          // Asynchronous Reset
+        end
+        4'b10xx: begin
+            Q <= D;                // Load Parallel Input
+        end
+        4'b1111: begin
+            Q <= Q + 1;            // Count Up
+        end
+        default: begin
+            Q <= Q;                // Hold State
+        end
+    endcase
+    RCO <= ENT && (&Q);
+end
+
+endmodule
