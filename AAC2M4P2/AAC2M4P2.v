05ac6a3ab24098a02bfaebab89ee24dcdcf20ee9
CeSium <fedir.katushonok@gmail.com>
2026-01-01 09:31:52 +0200

    refactor RAM128x32 module for improved structure and clarity

--- AAC2M4P2/AAC2M4P2.v
+++ AAC2M4P2/AAC2M4P2.v
@@ -10,3 +10,20 @@ module RAM128x32
     input wire [(Data_width-1):0] d,
     output wire [(Data_width-1):0] q
   );
+
+  reg [(Data_width-1):0] RAM[(2**Addr_width)-1:0]; //declare RAM
+  reg [(Data_width-1):0] out_reg;
+
+  assign q = out_reg; //output register
+
+  always @(posedge clk) begin
+    if (we) begin
+      RAM[address] <= d; //write data to RAM
+      out_reg <= {Data_width{1'bz}}; // write high impedance on output during write
+    end
+    else begin
+      out_reg <= RAM[address]; //read data from RAM
+    end
+  end
+
+endmodule
